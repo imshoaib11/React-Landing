0b77d18cbe629d3720055b84911f292d25475861
imshoaib11 <shoaib7337r.k.o@gmail.com>
2024-06-27 22:57:24 +0530

    Task completed

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Showcase from './components/Showcase'
+import NavBar from './components/NavBar'
+import Header from './components/Header'
+import Icon from './components/Icon'
+import Testimonials from './components/Testimonials'
+import Signin from './components/Signin'
+import Footer from './components/Footer'
+
+function App() {
+  return <>
+
+       <NavBar/>
+       <Header/>
+       <Icon/>
+      <Showcase/>
+      <Testimonials/>
+      <Signin/>
+      <Footer/>
+
+
+
+
+
+  </>
+}
+
+export default App
